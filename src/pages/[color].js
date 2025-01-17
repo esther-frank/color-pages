a01d9a21b3eb061ea0670977e3773ebf18646fe6
Esther Frank <185348393+esther-frank@users.noreply.github.com>
2025-01-17 12:43:29 +0000

    prettier formatting

--- src/pages/[color].js
+++ src/pages/[color].js
@@ -1,28 +1,30 @@
-import colors from '../data/colors.json';
-import BackButton from '../components/BackButton';
+import colors from "../data/colors.json"
+import BackButton from "../components/BackButton"
 
-export async function getStaticPaths () {
-    // loop through color array
-    const paths = colors.map(color => ({
-        // return an object with params.color set to the color's name
-        params: { color: color.name }
-    }))
-    return {paths, fallback: false}
-}
-    
-export async function getStaticProps ({params}) {
-    // find the info for just one color
-    const color = colors.find(color => color.name === params.color)
-    // return it in the necessary format
-    return {props: {color}}
+export async function getStaticPaths() {
+  // loop through color array
+  const paths = colors.map((color) => ({
+    // return an object with params.color set to the color's name
+    params: { color: color.name }
+  }))
+  return { paths, fallback: false }
 }
 
-export default function ColorPage ({color}) {
-    return (
-        <div style={{backgroundColor: color.hex}} className='p-8 flex flex-col gap-8 h-full'>
-            <h1 className='text-4xl font-extrabold'>{color.name}</h1>
-            <p className='text-2xl font-medium'>Hex code: {color.hex}</p>
-            <BackButton />
-        </div>
-    )
+export async function getStaticProps({ params }) {
+  // find the info for just one color
+  const color = colors.find((color) => color.name === params.color)
+  // return it in the necessary format
+  return { props: { color } }
+}
+
+export default function ColorPage({ color }) {
+  return (
+    <div
+      style={{ background: color.hex }} className="p-8 flex flex-col gap-8 h-full"
+    >
+      <h1 className="text-4xl font-extrabold">{color.name}</h1>
+      <p className="text-2xl font-medium">Hex code: {color.hex}</p>
+      <BackButton />
+    </div>
+  )
 }
