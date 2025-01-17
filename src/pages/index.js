f0048ad48399081f1b1c508862d7dc5ad3e26221
Esther Frank <185348393+esther-frank@users.noreply.github.com>
2025-01-17 09:42:28 +0000

    install prettier, improve styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
-import colors from '../data/colors.json';
-import Link from 'next/link';
+import colors from "../data/colors.json"
+import Link from "next/link"
 
 export default function Home() {
-    return (
-        <div className='flex flex-col items-center justify-center'>
-            <h1 className='text-green-700 text-4xl font-extrabold py-6'>Color list</h1>
-            <ul className='max-w-7xl text-left'>
-                {colors.map(color => (
-                    <li className='py-2 list-none' key={color.name}>
-                        <Link href={`/${color.name}`}>
-                            <div>{color.name}</div>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div>
+        <h1 className="text-green-700 text-5xl text-center font-extrabold py-6">
+          Color list
+        </h1>
+        <ul className="text-left">
+          {colors.map((color) => (
+            <li className="py-2 list-none text-lg" key={color.name}>
+              <Link href={`/${color.name}`}>
+                <div>{color.name}</div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
